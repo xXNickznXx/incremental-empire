09a7f386412830782938f9141d8f0e9041e9a3be
ue82381 <ue82381@sbb.ch>
2018-09-25 09:05:49 +0200

    design crafting

--- js.js
+++ js.js
@@ -93,7 +93,9 @@ $(document).ready(function () {
         speed,
 
         task = $("#task"),
-        msg = $("#msg");
+        msg = $("#msg"),
+        
+        win = window.matchMedia("(max-width: 376px)");
 
     /* Jobs */
     $("#up_gatherer").click(function () {
@@ -206,7 +208,9 @@ $(document).ready(function () {
 
     /* Navigation */
     $("#btn_build").click(function () {
-        $("#right").slideUp();
+        if (win.matches) {
+            $("#right").slideUp();
+        }
         $("#build").slideToggle();
         $("#upgrade").slideUp();
         $("#craft").slideUp();
@@ -215,7 +219,9 @@ $(document).ready(function () {
         $("#options").slideUp();
     });
     $("#btn_upgrade").click(function () {
-        $("#right").slideUp();
+        if (win.matches) {
+            $("#right").slideUp();
+        }
         $("#build").slideUp();
         $("#upgrade").slideToggle();
         $("#craft").slideUp();
@@ -224,7 +230,9 @@ $(document).ready(function () {
         $("#options").slideUp();
     });
     $("#btn_craft").click(function () {
-        $("#right").slideUp();
+        if (win.matches) {
+            $("#right").slideUp();
+        }
         $("#build").slideUp();
         $("#upgrade").slideUp();
         $("#craft").slideToggle();
@@ -233,7 +241,9 @@ $(document).ready(function () {
         $("#options").slideUp();
     });
     $("#btn_trade").click(function () {
-        $("#right").slideUp();
+        if (win.matches) {
+            $("#right").slideUp();
+        }
         $("#build").slideUp();
         $("#upgrade").slideUp();
         $("#craft").slideUp();
@@ -242,7 +252,9 @@ $(document).ready(function () {
         $("#options").slideUp();
     });
     $("#btn_travel").click(function () {
-        $("#right").slideUp();
+        if (win.matches) {
+            $("#right").slideUp();
+        }
         $("#build").slideUp();
         $("#upgrade").slideUp();
         $("#craft").slideUp();
@@ -251,7 +263,9 @@ $(document).ready(function () {
         $("#options").slideUp();
     });
     $("#btn_options").click(function () {
-        $("#right").slideUp();
+        if (win.matches) {
+            $("#right").slideUp();
+        }
         $("#build").slideUp();
         $("#upgrade").slideUp();
         $("#craft").slideUp();
@@ -876,18 +890,53 @@ $(document).ready(function () {
     
     /* Crafting */
     $("#btn_armor").click(function () {
+        if ($("#coll_armor").css("display") === "none") {
+            $("#btn_armor").css("border-bottom", "0");
+            $("#btn_armor").css("border-bottom-left-radius", "0");
+            $("#btn_armor").css("border-bottom-right-radius", "0");
+        } else {
+            $("#btn_armor").removeAttr("style");
+        }
         $("#coll_armor").slideToggle();
     });
     $("#btn_weapons").click(function () {
+        if ($("#coll_weapons").css("display") === "none") {
+            $("#btn_weapons").css("border-bottom", "0");
+            $("#btn_weapons").css("border-bottom-left-radius", "0");
+            $("#btn_weapons").css("border-bottom-right-radius", "0");
+        } else {
+            $("#btn_weapons").removeAttr("style");
+        }
         $("#coll_weapons").slideToggle();
     });
     $("#btn_melee").click(function () {
+        if ($("#coll_melee").css("display") === "none") {
+            $("#btn_melee").css("border-bottom", "0");
+            $("#btn_melee").css("border-bottom-left-radius", "0");
+            $("#btn_melee").css("border-bottom-right-radius", "0");
+        } else {
+            $("#btn_melee").removeAttr("style");
+        }
         $("#coll_melee").slideToggle();
     });
     $("#btn_ranged").click(function () {
+        if ($("#coll_ranged").css("display") === "none") {
+            $("#btn_ranged").css("border-bottom", "0");
+            $("#btn_ranged").css("border-bottom-left-radius", "0");
+            $("#btn_ranged").css("border-bottom-right-radius", "0");
+        } else {
+            $("#btn_ranged").removeAttr("style");
+        }
         $("#coll_ranged").slideToggle();
     });
     $("#btn_gifts").click(function () {
+        if ($("#coll_gifts").css("display") === "none") {
+            $("#btn_gifts").css("border-bottom", "0");
+            $("#btn_gifts").css("border-bottom-left-radius", "0");
+            $("#btn_gifts").css("border-bottom-right-radius", "0");
+        } else {
+            $("#btn_gifts").removeAttr("style");
+        }
         $("#coll_gifts").slideToggle();
     });
 
