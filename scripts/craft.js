5a947e8a61d789b0b81a064eeced6c8727894f29
ue82381 <ue82381@sbb.ch>
2018-09-26 10:14:25 +0200

    added tooltips craft

--- scripts/craft.js
+++ scripts/craft.js
@@ -5,53 +5,18 @@
 
 
 $("#btn_armor").click(function () {
-    if ($("#coll_armor").css("display") === "none") {
-        $("#btn_armor").css("border-bottom", "0");
-        $("#btn_armor").css("border-bottom-left-radius", "0");
-        $("#btn_armor").css("border-bottom-right-radius", "0");
-    } else {
-        $("#btn_armor").removeAttr("style");
-    }
     $("#coll_armor").slideToggle();
 });
 $("#btn_weapons").click(function () {
-    if ($("#coll_weapons").css("display") === "none") {
-        $("#btn_weapons").css("border-bottom", "0");
-        $("#btn_weapons").css("border-bottom-left-radius", "0");
-        $("#btn_weapons").css("border-bottom-right-radius", "0");
-    } else {
-        $("#btn_weapons").removeAttr("style");
-    }
     $("#coll_weapons").slideToggle();
 });
 $("#btn_melee").click(function () {
-    if ($("#coll_melee").css("display") === "none") {
-        $("#btn_melee").css("border-bottom", "0");
-        $("#btn_melee").css("border-bottom-left-radius", "0");
-        $("#btn_melee").css("border-bottom-right-radius", "0");
-    } else {
-        $("#btn_melee").removeAttr("style");
-    }
     $("#coll_melee").slideToggle();
 });
 $("#btn_ranged").click(function () {
-    if ($("#coll_ranged").css("display") === "none") {
-        $("#btn_ranged").css("border-bottom", "0");
-        $("#btn_ranged").css("border-bottom-left-radius", "0");
-        $("#btn_ranged").css("border-bottom-right-radius", "0");
-    } else {
-        $("#btn_ranged").removeAttr("style");
-    }
     $("#coll_ranged").slideToggle();
 });
 $("#btn_gifts").click(function () {
-    if ($("#coll_gifts").css("display") === "none") {
-        $("#btn_gifts").css("border-bottom", "0");
-        $("#btn_gifts").css("border-bottom-left-radius", "0");
-        $("#btn_gifts").css("border-bottom-right-radius", "0");
-    } else {
-        $("#btn_gifts").removeAttr("style");
-    }
     $("#coll_gifts").slideToggle();
 });
 
